d14b1379aa2d56d20b1d8c7d85e7ea64e47b08d4
Baltazar Villegas <balville1@publicisgroupe.net>
2020-04-29 12:14:06 -0400

    next.config.js back to next example https://github.com/zeit/next.js/blob/canary/examples/with-ant-design-less/next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const withPlugins = require('next-compose-plugins');
 const withLess = require('@zeit/next-less');
-const withImages = require('next-images');;
-// const withSass = require('@zeit/next-sass');
 
 // for antd less theme
 const lessToJS = require('less-vars-to-js');
 const fs = require('fs')
 const path = require('path')
+
+const withImages = require('next-images');;
+// const withSass = require('@zeit/next-sass');
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
@@ -22,13 +23,30 @@ const plugins = [
   withLess({
     lessLoaderOptions: {
       javascriptEnabled: true,
-      // modifyVars: themeVariables, // make your antd custom effective
+      modifyVars: themeVariables, // make your antd custom effective
     },
-    webpack: (config) => {
-      if (typeof require !== 'undefined') {
-        require.extensions['.less'] = file => {}
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style.*?/
+        const origExternals = [...config.externals]
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback()
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback)
+            } else {
+              callback()
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ]
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        })
       }
-      return config;
+      return config
     },
   }),
 
